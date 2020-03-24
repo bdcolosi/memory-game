c7466c890aba6a4dd5907d0eead38fbe2520ef77
bdcolosi <bdcolosi27@gmail.com>
2020-03-24 12:04:14 -0400

    added step 5

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
 
+import "./MemoryCard.css";
+
 class MemoryCard extends Component {
     render() {
         return (
-            <h1>CARD</h1>
+            <div className="MemoryCard">
+                <img class="image" src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"/>
+            </div>
         )
     }
 }
