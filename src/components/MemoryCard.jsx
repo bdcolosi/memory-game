f03e1bca5cba488609f97745603343b304601aec
bdcolosi <bdcolosi27@gmail.com>
2020-03-25 10:38:09 -0400

    step 1, having trouble with rendering cards correctly

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -2,14 +2,21 @@ import React, {Component} from 'react';
 
 import "./MemoryCard.css";
 
-class MemoryCard extends Component {
+class MemoryCardBack extends Component {
     render() {
         return (
             <div className="MemoryCard">
+            <div className="MemoryCardInner">
+            <div className="MemoryCardBack">
                 <img class="image" src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png"/>
+            </div>
+            <div className="MemoryCardFront">
+               ∆ 
+            </div>
+            </div>
             </div>
         )
     }
 }
 
-export default MemoryCard;
+export default MemoryCardBack;
